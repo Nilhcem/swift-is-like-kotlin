0bdccc11e9f9c2f67b02db5de1ae24eb0b4f5e4f
Robert M. Ryan <robert.ryan@mindspring.com>
2018-02-22 09:39:26 -0800

    Fix function syntax and show alternatives
    The function return type syntax in Swift is `(Int) -> Int`, not `(Int
    -> Int)`. Or, if you want to make it clear, you could do
    
    ```
    func makeIncrementor() -> ((Int) -> Int) { … }
    ```
    
    Also, include some shorter permutations to make it more comparable to
    the “shorter” alternative provided for Kotlin example.

--- code/function-type.swift
+++ code/function-type.swift
@@ -1,8 +1,21 @@
-func makeIncrementer() -> (Int -> Int) {
-    func addOne(number: Int) -> Int {
+func makeIncrementer() -> (Int) -> Int {
+    func addOne(_ number: Int) -> Int {
         return 1 + number
     }
     return addOne
 }
 let increment = makeIncrementer()
 increment(7)
+
+// incrementer can also be written in a shorter way:
+
+func makeIncrementer() -> (Int) -> Int {
+    return { (number: Int) -> Int in 1 + number }
+}
+
+let increment = makeIncrementer()
+
+// or even shorter:
+
+let increment = { (number: Int) -> Int in 1 + number }
+
